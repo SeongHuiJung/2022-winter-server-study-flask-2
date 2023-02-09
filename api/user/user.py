c4eea10c52466bd758bad2cae071e19ddd7875b0
SeongHuiJung <tjdgml150237@naver.com>
2023-02-09 13:47:37 +0900

    과제 수정2

--- api/user/user.py
+++ api/user/user.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 #from flask import Flask, request, jsonify
 
 from flask_restx import Resource, Namespace
@@ -25,11 +25,11 @@ class UserManagement(Resource):
             #해당 유저 존재
             if(row is not None):
                 result={'message':'아이디나 비밀번호 불일치'}
+                return result, 400
             #해당 유저 없음
             else:
                 result={'message':'해당 유저가 존재하지 않음'}
-
-        return result
+                return result, 400
 
     def post(self):
         # POST method 구현 부분
@@ -43,14 +43,16 @@ class UserManagement(Resource):
         if(row is not None):
             result={'is_success':False,
                     'message':'이미 있는 유저'}
+            return result, 400
         else:
             row=database.execute_one("INSERT INTO SeongHui.user VALUES ('%s','%s','%s');" %(data['id'], data['pw'], data['nickname']))
             result={'is_success':True,
                     'message':'유저 생성 성공'}
             database.commit()
             database.close()
+            return result
             
-        return result
+        
 
     def put(self):
         # PUT method 구현 부분
@@ -65,19 +67,19 @@ class UserManagement(Resource):
             if(row is not None):
                 result={'is_success':False,
                     'message':'현재 닉네임과 같음'}
+                return result,400
             else:
                 database.execute_one("UPDATE SeongHui.user SET nickname='%s' WHERE id = '%s' AND pw = '%s';" %(data['nickname'], data['id'], data['pw']))
                 database.commit()
                 database.close()
                 result={'is_success':True,
                     'message':'유저 닉네임 변경 성공'}
+                return result
         
         else:
             result={'is_success':False,
                     'message':'아이디나 비밀번호 불일치'}
-
-
-        return result
+            return result,400
     
     def delete(self):
         # DELETE method 구현 부분
@@ -93,8 +95,8 @@ class UserManagement(Resource):
                     'message':'유저 삭제 성공'}
             database.commit()
             database.close()
+            return result
         else:
             result={'is_success':False,
                     'message':'아이디나 비밀번호 불일치'}
-
-        return result
+            return result, 400
