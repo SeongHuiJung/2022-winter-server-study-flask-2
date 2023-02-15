e398d340c9974bb1db8c88ff5017ee06aa4e7f0d
SeongHuiJung <tjdgml150237@naver.com>
2023-02-15 09:36:20 +0900

    유니지4

--- unity_ubuntu.py
+++ unity_ubuntu.py
@@ -1,27 +1,12 @@
 from flask import Flask, request
-from flask_restx import Resource, Namespace
-from database.database import Database
+
 
 app = Flask(__name__)
 
 
 @app.route('/',methods=['GET','POST'])
 def connect():
-    database=Database()
-    #id=request.form['id']
-    #password=request.form['password']
-    id=request.args.get('id')
-    password=request.args.get('password')
-
-    row=database.execute_one("SELECT * FROM SeongHui.user WHERE id = '%s' AND pw = '%s';" %(id,password))
-
-
-    #아이디 비밀번호 일치
-    if(row is not None):
-        return 1
-    else:
-        return 0
-
+    return "연결성공"
 
 
 if __name__ == "__main__":
